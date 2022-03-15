c6e6f8215646a4914205204a58d1f94010b0bbff
Elvira Mak <efilia@bk.ru>
2022-03-15 21:33:53 +0400

    Создан тест для Контактов

---
+++ contact.py
@@ -0,0 +1,9 @@
+
+
+class Contact:
+    def __init__(self, name, middle_name, company, home_phone, email):
+        self.name = name
+        self.middle_name = middle_name
+        self.company = company
+        self.home_phone = home_phone
+        self.email = email
